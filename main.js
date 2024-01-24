0a4136463500216732c8521cd586fa01900a1fa3
Brendan Muhia <109690494+Xmuhia@users.noreply.github.com>
2024-01-24 23:27:49 +0300

    Improving Code Readability

--- main.js
+++ main.js
@@ -1,53 +1,64 @@
-
 const result = document.getElementById('result');
-const sound =  document.getElementById('sound');
-const btn =  document.getElementById('search-btn');
-let searchQuery='';
-let submit = document.getElementById('myForm');
+const sound = document.getElementById('sound');
+const searchBtn = document.getElementById('search-btn');
+const inputWord = document.getElementById('inp-word');
+const myForm = document.getElementById('myForm');
 
-//==========SUBMIT BUTTON=============
-btn.addEventListener('click', () => {
-    searchQuery =  document.getElementById('inp-word').value;
-    fetchAPI();
-});
+// Event listeners
+searchBtn.addEventListener('click', handleSearch);
+myForm.addEventListener('submit', handleSubmit);
 
-//==========SUBMIT ENTER=============
-submit.addEventListener('submit', (e) => {
-    e.preventDefault();
-    searchQuery =  document.getElementById('inp-word').value;
-    fetchAPI();
-});
+// Functions
+async function handleSearch() {
+  const searchQuery = inputWord.value;
+  await fetchAPI(searchQuery);
+}
 
-//==========FETCH API=============
-async function fetchAPI () {
+async function handleSubmit(e) {
+  e.preventDefault();
+  const searchQuery = inputWord.value;
+  await fetchAPI(searchQuery);
+}
+
+async function fetchAPI(searchQuery) {
+  try {
     const baseURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchQuery}`;
     const response = await fetch(baseURL);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
     const data = await response.json();
-    console.log(data);
-    result.innerHTML = 
-    `
+    updateUI(data);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+function updateUI(data) {
+  const wordDetails = `
     <div class="word">
-                <h3>${data[0].word} </h3>
-                <button onclick='playSound()' id="search-btn"><i class='bx bxs-volume-full'></i></button>
-            </div>
-            <div class="details">
-                <p>${data[0].meanings[0].partOfSpeech}</p>
-                <p>${data[0].phonetic}</p>
-            </div>
-                <p class="word-meaning">
-                <li>${data[0].meanings[0].definitions[0].definition}</li>
-                <li>${data[0].meanings[0].definitions[1].definition}</li>
-                <li>${data[0].meanings[0].definitions[2].definition}</li>
-                </p>
-                <p class="word-example">
-                ${data[0].meanings[0].definitions[0].example}
-                </p>
-    `
-    sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
-    console.log(sound);
+      <h3>${data[0].word}</h3>
+      <button onclick='playSound()' id="result-btn"><i class='bx bxs-volume-full'></i></button>
+    </div>
+    <div class="details">
+      <p>${data[0].meanings[0].partOfSpeech}</p>
+      <p>${data[0].phonetic}</p>
+    </div>
+    <p class="word-meaning">
+      <li>${data[0].meanings[0].definitions[0].definition}</li>
+      <li>${data[0].meanings[0].definitions[1].definition}</li>
+      <li>${data[0].meanings[0].definitions[2].definition}</li>
+    </p>
+    <p class="word-example">
+      ${data[0].meanings[0].definitions[0].example}
+    </p>
+  `;
+  result.innerHTML = wordDetails;
+  sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
 }
 
 function playSound() {
-    sound.play();
+  sound.play();
 }
-
